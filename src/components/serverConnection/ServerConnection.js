c30998d226ee1bfd543992de1d44c5c57975799b
kiotobat <lambar926@gmail.com>
2025-08-09 22:19:43 +0300

    edit backend again

--- src/components/serverConnection/ServerConnection.js
+++ src/components/serverConnection/ServerConnection.js
@@ -4,7 +4,7 @@ export default class ServerConnection {
   constructor() {
     this.message = document.createElement('div');
     this.message.classList.add('server-connection');
-    this.message.textContent = 'Сервер не найден :(';
+    this.message.textContent = 'Сервер не найден';
   }
 
   render(container) {
